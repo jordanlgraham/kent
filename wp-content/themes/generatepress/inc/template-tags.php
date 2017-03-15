213e10c6f14099f54b0d0e4bf26dce1b6b239b2f
Jordan Graham <jordan@trivids.com>
2017-03-15 05:42:40 +0000

    Patched generatepress theme and created patchfile.

--- wp-content/themes/generatepress/inc/template-tags.php
+++ wp-content/themes/generatepress/inc/template-tags.php
@@ -537,11 +537,80 @@ function generate_header_items()
 	// Header widget
 	generate_construct_header_widget();
 	
-	// Site title and tagline
-	generate_construct_site_title();
-	
 	// Site logo
-	generate_construct_logo();
+	// generate_construct_logo();
+	
+	// Site title and tagline
+	// generate_construct_site_title();
+    
+    // New function
+    generate_construct_title_logo();
+}
+endif;
+
+if ( ! function_exists( 'generate_construct_logo' ) ) :
+/**
+ * Build the logo
+ *
+ * @since 1.3.28
+ */
+function generate_construct_title_logo()
+{
+  $generate_settings = wp_parse_args( 
+    get_option( 'generate_settings', array() ), 
+    generate_get_defaults() 
+  );
+  
+  // Get the title and tagline
+  $title = get_bloginfo( 'title' );
+  $tagline = get_bloginfo( 'description' );
+  
+  // If the disable title checkbox is checked, or the title field is empty, return true
+  $disable_title = ( '1' == $generate_settings[ 'hide_title' ] || '' == $title ) ? true : false; 
+  
+  // If the disable tagline checkbox is checked, or the tagline field is empty, return true
+  $disable_tagline = ( '1' == $generate_settings[ 'hide_tagline' ] || '' == $tagline ) ? true : false;
+  
+  // Get our logo URL if we're using the custom logo
+  $logo_url = ( function_exists( 'the_custom_logo' ) && get_theme_mod( 'custom_logo' ) ) ? wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' ) : false;
+  
+  // Get our logo from the custom logo or our GP setting
+  $logo = ( $logo_url ) ? $logo_url[0] : $generate_settings['logo'];
+  
+  ?>
+  <div class="site-branding">
+  <?php
+  
+  // Site title and tagline
+  if ( false == $disable_title || false == $disable_tagline ) : 
+    // If we don't have a logo, bail
+    if ( !empty( $logo ) ) {
+    
+    // Print our HTML
+      printf( 
+        '<div class="site-logo">
+          <a href="%1$s" title="%2$s" rel="home">
+            <img class="header-image" src="%3$s" alt="%2$s" title="%2$s" />
+          </a>
+        </div>',
+        apply_filters( 'generate_logo_href' , esc_url( home_url( '/' ) ) ),
+        apply_filters( 'generate_logo_title', esc_attr( get_bloginfo( 'name', 'display' ) ) ),
+        apply_filters( 'generate_logo', esc_url( $logo ) )
+      );
+    } ?>
+  
+
+      <?php if ( false == $disable_title ) : ?>
+        <div id="site-title"><p class="main-title" itemprop="headline"><a href="<?php echo apply_filters( 'generate_site_title_href', esc_url( home_url( '/' ) ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p></div>
+      <?php endif;
+        
+      if ( false == $disable_tagline ) : ?>
+        <p class="site-description"><?php echo html_entity_decode( bloginfo( 'description' ) ); ?></p>
+      <?php endif; ?>
+    </div>
+  <?php endif;
+  
+
 }
 endif;
 
